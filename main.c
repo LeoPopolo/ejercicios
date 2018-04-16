aee1979aac6f6006aed2fa7322710e79dd90c3c5
LeoPopolo <leopopologuitarreta@gmail.com>
2018-04-16 12:22:53 -0300

    ordenamiento estructuras

--- main.c
+++ main.c
@@ -1,18 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <conio.h>
-#include "funciones.h"
+
+typedef struct
+{
+    char nombre[20];
+    int legajo;
+    float sueldo;
+    char sexo;
+
+} eEmpleado;
+
+void mostrar(eEmpleado unEmpleado);
+void mostrarTodos(eEmpleado emp[]);
+void ordenar(eEmpleado emp[]);
 
 int main()
 {
-    eProgramador lista1 [] = {{.idProg = 1, .nombre = "Juan", .estado = 1},{.idProg = 2, .nombre = "Miguel", .estado = 1},{.idProg = 3, .nombre = "Maria", .estado = 1}};
-    eProyecto lista3 [] = {{.idProy = 1, .nombreProy = "Android", .hsProy = 50},{.idProy = 2, .nombreProy = "iOS", .hsProy = 65},{.idProy = 3, .nombreProy = "Windows Phone", .hsProy = 70}};
-    eCategoria lista2 [] = {{.idCat = 1, .nombreCat = "Junior", .valorPH = 50},{.idCat = 2, .nombreCat = "SemiSenior", .valorPH = 75},{.idCat = 3, .nombreCat = "Senior", .valorPH = 100}};
-    ePPC lista4 [] = {{ 1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
+    eEmpleado unEmpleado[3] = {{"Leo", 1234, 20000, 'm'},{"Juan", 1235, 15000, 'm'},{"Maria", 1236, 18000, 'f'}};
 
-    mostrarProgram(lista1, lista2, lista3 , lista4, 3, 3, 3);
+    printf("Nombre\tLegajo\tSueldo\t\tSexo\n\n");
+    printf("-------------------------------------------\n");
+
+    ordenar(unEmpleado);
+    mostrarTodos(unEmpleado);
+
+}
+
+void mostrar(eEmpleado unEmpleado)
+{
+
+    /* printf("ingrese nombre: ");
+     gets(unEmpleado.nombre);
+     printf("ingrese legajo: ");
+     scanf("%d", &unEmpleado.legajo);
+     printf("ingrese sueldo: ");
+     scanf("%f", &unEmpleado.sueldo);
+     printf("ingrese sexo: ");
+     fflush(stdin);
+     scanf("%c", &unEmpleado.sexo);
+     system("cls");*/
 
 
-    return 0;
+    printf("%s\t%d\t$%.2f\t%c\n", unEmpleado.nombre, unEmpleado.legajo, unEmpleado.sueldo, unEmpleado.sexo);
+}
+
+void mostrarTodos(eEmpleado emp[])
+{
+    for(int i=0; i<3; i++)
+    {
+        mostrar(emp[i]);
+    }
+}
+
+void ordenar(eEmpleado emp[])
+{
+    eEmpleado aux;
+
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            if(strcmp(emp[j].nombre, emp[i].nombre)>0)
+            {
+                strcpy(aux.nombre, emp[i].nombre);
+                strcpy(emp[i].nombre, emp[j].nombre);
+                strcpy(emp[j].nombre, aux.nombre);
+
+                aux.legajo = emp[i].legajo;
+                emp[i].legajo = emp[j].legajo;
+                emp[j].legajo = aux.legajo;
+
+                aux.sexo = emp[i].sexo;
+                emp[i].sexo = emp[j].sexo;
+                emp[j].sexo = aux.sexo;
+
+                aux.sueldo = emp[i].sueldo;
+                emp[i].sueldo = emp[j].sueldo;
+                emp[j].sueldo = aux.sueldo;
+            }
+        }
+    }
 }
